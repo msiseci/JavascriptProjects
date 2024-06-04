1c1daaa36a795467c8a0963f66d46bc96d95bad0
mervesiseci@gmail.com <mervesiseci@gmail.com>
2024-06-04 15:44:16 +0300

    add testimonial slider project

---
+++ testimonial-slider/script.js
@@ -0,0 +1,23 @@
+var slide = document.getElementById("slide");
+var arrowUp = document.getElementById("arrow-up");
+var arrowDown = document.getElementById("arrow-down");
+
+let x = 0;
+
+arrowUp.onclick = function() {
+    debugger
+   if (x > -900) {
+    x = x - 300; // because card height is 300px 
+    slide.style.top = x + "px";
+   }
+
+}
+
+arrowDown.onclick = function() {
+    debugger
+   if (x < 0) {
+    x = x + 300; // because card height is 300px 
+    slide.style.top = x + "px";
+   }
+
+}
